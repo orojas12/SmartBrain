cbcfc983e78c74f75acc2ffc999d2ed06524f2aa
oscar <orojas12@outlook.com>
2019-11-21 18:05:08 -0700

    added sign-in/sign-out forms

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import 'tachyons';
@@ -32,6 +34,8 @@ class App extends React.Component {
     this.state = {
       imageUrl: '',
       faceBox: {},
+      route: 'signin',
+      isSignedIn: false,
     }
   }
 
@@ -69,16 +73,34 @@ class App extends React.Component {
     this.setState({imageUrl: event.target.value})
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+    this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
+  }
+
   render() {
     return (
       <div className="App">
         <Particles className='particles' params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} faceBox={this.state.faceBox} />
-
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+        { this.state.route === 'home'
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+              <FaceRecognition imageUrl={this.state.imageUrl} faceBox={this.state.faceBox} />
+            </div>
+          : (
+            this.state.route === 'signin' 
+            ? <SignIn onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} />
+          )
+            
+        }
       {/* 
           <Navigation />
           <Logo />
