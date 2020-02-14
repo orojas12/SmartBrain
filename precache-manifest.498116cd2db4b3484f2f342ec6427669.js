71a1c32405906c2dccef18c19d185c33ce7477c1
orojas12 <orojas12@outlook.com>
2020-02-13 21:18:11 -0700

    Updates

---
+++ precache-manifest.498116cd2db4b3484f2f342ec6427669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398de77dda0b6eaaf180060ef28eb4e6",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "30b2e100b0a23eb25091",
+    "url": "/SmartBrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a329d3230b2446a3e39d",
+    "url": "/SmartBrain/static/css/main.c17f8c4c.chunk.css"
+  },
+  {
+    "revision": "30b2e100b0a23eb25091",
+    "url": "/SmartBrain/static/js/2.90970a27.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/SmartBrain/static/js/2.90970a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a329d3230b2446a3e39d",
+    "url": "/SmartBrain/static/js/main.af62298d.chunk.js"
+  },
+  {
+    "revision": "1d4d503b43fc737a0178",
+    "url": "/SmartBrain/static/js/runtime-main.a24ab6ac.js"
+  }
+]);
